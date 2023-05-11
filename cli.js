e56a1ac0e12994895e2923722eb83ab1ad184fd7
IgnaBascu <igna.bascu@gmail.com>
2023-05-11 13:38:00 -0400

    NPM install colors

--- cli.js
+++ cli.js
@@ -1,3 +1,5 @@
-console.log('Hello world')
-console.log('Fazt')
-console.log('Prueba loc@')
+const colors = require('colors')
+
+console.log('Hello world'.bgGreen)
+console.log('Fazt'.bgBlue)
+console.log('Prueba loc@'.bgMagenta)
